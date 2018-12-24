af3c79f87ed04de5d78cfc8340e119ab0b0cbf98
Navin Ayer <ayernavin@togglecorp.com>
2018-12-24 15:10:40 +0545

    Remove ravl from deploy
    * Add auto-restart for cern
    * Add environment type for cern i.e beta

--- deploy/scripts/travis_build.sh
+++ deploy/scripts/travis_build.sh
@@ -6,12 +6,10 @@ ROOT_DIR=$(dirname "$(dirname "$BASE_DIR")") # /code/
 DEEP_SERVER_REPO='https://github.com/the-deep/server.git'
 DEEP_CLIENT_REPO='https://github.com/the-deep/client.git'
 DEEP_REACT_STORE_REPO='https://github.com/the-deep/react-store'
-DEEP_RAVL_REPO='https://github.com/the-deep/ravl'
 
 SERVER_PATH=${ROOT_DIR}/server
 CLIENT_PATH=${ROOT_DIR}/client
 REACT_STORE_PATH=${CLIENT_PATH}/src/vendor/react-store
-RAVL_PATH=${CLIENT_PATH}/src/vendor/ravl
 DEPLOY_CONFIG_PATH=$ROOT_DIR/deploy-config.json
 
 # Ignore Pull requets
@@ -36,7 +34,6 @@ DEEP_SERVER_DEPLOY=`jq -r '.server.deploy' ${DEPLOY_CONFIG_PATH}`
 DEEP_CLIENT_DEPLOY=`jq -r '.client.deploy' ${DEPLOY_CONFIG_PATH}`
 
 DEEP_REACT_STORE_REPO=$(jq -r ".client.reactStoreRepo // \"${DEEP_REACT_STORE_REPO}\"" ${DEPLOY_CONFIG_PATH})
-DEEP_RAVL_REPO=$(jq -r ".client.ravlRepo // \"${DEEP_RAVL_REPO}\"" ${DEPLOY_CONFIG_PATH})
 
 DEEP_SERVER_BRANCH=`jq -r '.server.server' ${DEPLOY_CONFIG_PATH}`
 DEEP_CLIENT_BRANCH=`jq -r '.client.client' ${DEPLOY_CONFIG_PATH}`
@@ -68,9 +65,6 @@ if [ "${DEEP_CLIENT_DEPLOY,,}" = "true" ]; then
 
     git clone --branch=${DEEP_REACT_STORE_BRANCH} ${DEEP_REACT_STORE_REPO} ${REACT_STORE_PATH}
     git --git-dir=${REACT_STORE_PATH}/.git --no-pager show --pretty=fuller --quiet
-
-    git clone --branch=${DEEP_RAVL_BRANCH} ${DEEP_RAVL_REPO} ${RAVL_PATH}
-    git --git-dir=${RAVL_PATH}/.git --no-pager show --pretty=fuller --quiet
 
     cp ${REACT_STORE_PATH}/stylesheets/_user-imports-sample.scss ${REACT_STORE_PATH}/stylesheets/_user-imports.scss
     docker pull thedeep/deep-client:latest
