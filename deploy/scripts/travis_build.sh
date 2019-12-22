bd917287c96ba4fddcd5772abb60b80edbb6ea78
tnagorra <weathermist@gmail.com>
2019-12-22 16:27:34 +0545

    Add re-map

--- deploy/scripts/travis_build.sh
+++ deploy/scripts/travis_build.sh
@@ -6,10 +6,12 @@ ROOT_DIR=$(dirname "$(dirname "$BASE_DIR")") # /code/
 DEEP_SERVER_REPO='https://github.com/the-deep/server.git'
 DEEP_CLIENT_REPO='https://github.com/the-deep/client.git'
 DEEP_REACT_STORE_REPO='https://github.com/the-deep/react-store'
+DEEP_RE_MAP_REPO='https://github.com/the-deep/re-map'
 
 SERVER_PATH=${ROOT_DIR}/server
 CLIENT_PATH=${ROOT_DIR}/client
 REACT_STORE_PATH=${CLIENT_PATH}/src/vendor/react-store
+RE_MAP_PATH=${CLIENT_PATH}/src/vendor/re-map
 DEPLOY_CONFIG_PATH=$ROOT_DIR/deploy-config.json
 
 # Ignore Pull requets
@@ -34,10 +36,12 @@ DEEP_SERVER_DEPLOY=`jq -r '.server.deploy' ${DEPLOY_CONFIG_PATH}`
 DEEP_CLIENT_DEPLOY=`jq -r '.client.deploy' ${DEPLOY_CONFIG_PATH}`
 
 DEEP_REACT_STORE_REPO=$(jq -r ".client.reactStoreRepo // \"${DEEP_REACT_STORE_REPO}\"" ${DEPLOY_CONFIG_PATH})
+DEEP_RE_MAP_REPO=$(jq -r ".client.reMapRepo // \"${DEEP_RE_MAP_REPO}\"" ${DEPLOY_CONFIG_PATH})
 
 DEEP_SERVER_BRANCH=`jq -r '.server.server' ${DEPLOY_CONFIG_PATH}`
 DEEP_CLIENT_BRANCH=`jq -r '.client.client' ${DEPLOY_CONFIG_PATH}`
 DEEP_REACT_STORE_BRANCH=`jq -r '.client.reactStore' ${DEPLOY_CONFIG_PATH}`
+DEEP_RE_MAP_BRANCH=`jq -r '.client.reMap' ${DEPLOY_CONFIG_PATH}`
 set +e
 
 set -ex
@@ -64,6 +68,9 @@ if [ "${DEEP_CLIENT_DEPLOY,,}" = "true" ]; then
 
     git clone --branch=${DEEP_REACT_STORE_BRANCH} ${DEEP_REACT_STORE_REPO} ${REACT_STORE_PATH}
     git --git-dir=${REACT_STORE_PATH}/.git --no-pager show --pretty=fuller --quiet
+
+    git clone --branch=${DEEP_RE_MAP_BRANCH} ${DEEP_RE_MAP_REPO} ${RE_MAP_PATH}
+    git --git-dir=${RE_MAP_PATH}/.git --no-pager show --pretty=fuller --quiet
 
     docker pull thedeep/deep-client:latest
     docker build --cache-from thedeep/deep-client:latest --tag thedeep/deep-client:latest ${CLIENT_PATH}
